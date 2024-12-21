539a341a0fec5bad692e2d94a79a9e23f99b2839
awash-dev <mohammedhusse4242@gmail.com>
2024-12-21 23:37:36 +0300

    some error in id boolen

--- app/Home/product/_layout.tsx
+++ app/Home/product/_layout.tsx
@@ -1,12 +1,24 @@
-import React from 'react'
-import { Stack } from 'expo-router'
+import React from 'react';
+import { Link, Stack } from 'expo-router';
+import { TouchableOpacity, Text } from 'react-native';
+import { Ionicons } from '@expo/vector-icons'; // Import Ionicons or any icon library
 
 const _layout = () => {
     return (
         <Stack>
-            <Stack.Screen name='[id]'  options={{ headerShown: true }} />
+            <Stack.Screen
+                name='[id]'
+                options={{
+                    headerShown: true,
+                    headerLeft: () => (
+                        <Link href={'Home/screen/Home'} style={{marginRight:25, marginLeft:10}} >
+                            <Ionicons name="arrow-back" size={24} color="black" />
+                        </Link>
+                    ),
+                }}
+            />
         </Stack>
-    )
+    );
 }
 
-export default _layout
+export default _layout;
